d93b65dd291e756c6045d81640d620eaa1d6278a
Fucloud <270928474@qq.com>
2023-08-30 22:24:56 +0800

    feat: 接入gpt3.5

---
+++ gpt/config.py
@@ -0,0 +1,18 @@
+# -*- coding = utf-8 -*-
+# @Time : 2023/08/30 22:13
+# @Autor : Fucloud
+# @FIle : config.py.py
+# @Software : PyCharm
+
+import json
+
+
+def read_config() -> dict:
+    file_path = "config.json"
+    with open(file_path, "r") as f:
+        config_text = '\n'.join(f.readlines(-1))
+
+    config_json = json.loads(config_text)
+
+    return config_json
+
