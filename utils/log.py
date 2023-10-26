8ebb879355d25223ce9dd050e514ad25b9be586c
Fucloud <270928474@qq.com>
2023-10-27 01:39:45 +0800

    perf: 优化Log对象

--- utils/log.py
+++ utils/log.py
@@ -1,17 +1,27 @@
 from utils import time
 
 class ScheduleLog:
-    def __init__(self):
+    def __init__(self, with_start=False):
         self.__begin_time = None
+        self.__is_start = False
+        
+        # 自动开启计时
+        if with_start:
+            self.start()
 
     def start(self):
-        self.__begin_time = time.now()
+        if not self.__is_start:
+            self.__begin_time = time.now()
+            self.__is_start = True
+        else:
+            raise RuntimeWarning("The sceduleLog has started!")
+        
 
     def clear(self):
         self.__begin_time = None
 
     def log(self, info):
-        print("[%2.3fs] %s" % (self.cur_time, info))
+        print("[%02.2fs] %s" % (self.cur_time, info))
 
     @property
     def cur_time(self):
