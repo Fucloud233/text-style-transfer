58488c625504c8378de3304fd09ef217898d2bed
maillab017 <mailab017@user-NF8480M6>
2023-10-29 17:15:19 +0800

    perf: 优化风格迁移函数调用逻辑

--- src/transfer.py
+++ src/transfer.py
@@ -1,16 +1,18 @@
+import sys
+sys.path.append(".")
+
 import json
 import random 
 import fire
-from enum import IntEnum
 
+from tqdm import tqdm
+
+from utils.config import TransferConfig, LoadType
+from utils.file import read_json
 from utils.log import ScheduleLog
 from llama2 import Llama2, LlamaType
 
 PROMPT = "There is a sentence '{}'. You should rewrite it more positive. The more positive sentence is {{"\
-
-class LoadType(IntEnum):
-    Front = 0, 
-    Random = 1,
 
 def load_dataset(dataset_path: str, k: int, load_type: LoadType=LoadType.Front):
     with open(dataset_path, 'r', encoding='utf-8') as f:
@@ -38,31 +40,28 @@ def save_result_with_input(input: list, result: list, output_path: str):
         #     f.write(f'"{sentence}"')
         f.write(json.dumps(merge, indent=4))
 
-def transfer_7b_chat_yelp(k: int = -1):
-    if k == "":
+def transfer_7b_chat_yelp(config: TransferConfig):
+    if config.k == "":
         print("The size of dataset to transfer no set!")
         return 
-
-    dataset_path = 'data/yelp/sentiment.test.0'
-    output_path = 'output/7b_chat_yelp/test.0.json'
     
     s_log = ScheduleLog(); s_log.start()
     s_log.log('Loading Dataset.')
     
     # 读取k条数据集
-    dataset = load_dataset(dataset_path, k)    
+    dataset = load_dataset(config.dataset_path, config.k, config.load_type)    
     s_log.log(f'Load over. (size = {len(dataset)}).')
 
     # 使用Llama转换
     bot = Llama2(PROMPT, LlamaType.Llama_7B_Chat) 
     transfer_result = []
-    for sentence in dataset:
+    for sentence in tqdm(dataset, desc="Dataset: "):
         transfer_result.append(bot.transfer(sentence))
     s_log.log('Transfer over.')
 
     # 保存数据
     # save_result(transfer_result, output_path)
-    save_result_with_input(dataset, transfer_result, output_path)
+    save_result_with_input(dataset, transfer_result, config.output_path)
     s_log.log('Save over.')
 
 def main():
@@ -76,6 +75,11 @@ def main():
         result = bot.transfer(sentence)
         print("bot:", result)
 
-
+def run(path: str):
+    transfer_config = TransferConfig.from_file(path)
+    transfer_7b_chat_yelp(transfer_config)
+    
+    
 if __name__ == '__main__':
-    fire.Fire(transfer_7b_chat_yelp)
+    fire.Fire(run)
+    # fire.Fire(transfer_7b_chat_yelp)
