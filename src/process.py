0258d4c9ea50348835c81c7fe37203dbefb3b4dc
Fucloud-arch <270928474@qq.com>
2023-10-29 17:42:16 +0800

    perf: 优化数据处理函数

--- src/process.py
+++ src/process.py
@@ -10,24 +10,26 @@ def save_sentence(file_path: str, sentences):
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(sentences, f, indent=4)
     
-def process():
-    raw_file_path = 'output/7b_chat_yelp/test.0.raw.json'
-    output_file_path = 'output/7b_chat_yelp/test.0.json'
-
+def process(raw_file_path: str, output_file_path: str):
     raw_sentences = load_sentences(raw_file_path)
 
     sentences = []
     for r_s in raw_sentences:
         sentences.append({
-            '0': r_s['0'].split(END_SYMBOL)[0],
-            '1': r_s['1'].split(END_SYMBOL)[0]
+            '0': r_s['0'].split(END_SYMBOL)[0].split('\n')[0],
+            '1': r_s['1'].split(END_SYMBOL)[0].split('\n')[0]
         })
 
     save_sentence(output_file_path, sentences)
     
     print('Save Over!')
 
+def main():
+    raw_file_path = 'output/7b_chat_yelp/test.0/100/transfer.raw.json'
+    output_file_path = 'output/7b_chat_yelp/test.0/100/transfer.json'
+
+    process(raw_file_path, output_file_path)
+
 if __name__ == '__main__':
-    process()
-    
+    main();    
     
