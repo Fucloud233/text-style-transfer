6abff99fff796917bdd99a4d4e06a1a9e2a19d61
Fucloud-arch <270928474@qq.com>
2023-10-29 17:41:49 +0800

    perf: 优化评测启动脚本

--- script/evaluate.sh
+++ script/evaluate.sh
@@ -1,1 +1,21 @@
-python3 src/evaluate.py data/eval_config/7b_chat_yelp.test.0.json
+# python3 src/evaluate.py data/eval_config/7b_chat_yelp.test.0.json
+
+while getopts p: flag
+
+do
+    case "${flag}" in
+        p) path=${OPTARG};;
+    esac
+done
+
+folder="output/log"
+output_file="$folder/eval.out"
+
+# echo $output_file
+
+if [ ! -d $folder ]; then
+    mkdir $folder
+fi
+
+# nohup ./myprogram > foo.out 2> foo.err < /dev/null &
+nohup python3 src/evaluate.py $path >$output_file 2> $output_file &disown
