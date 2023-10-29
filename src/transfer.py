5da7c1d2c47bab20fa2aebede03256e9ae7ce362
Fucloud-arch <270928474@qq.com>
2023-10-29 17:42:53 +0800

    perf: 优化迁移函数 合并处理函数

--- src/transfer.py
+++ src/transfer.py
@@ -8,11 +8,13 @@ import fire
 from tqdm import tqdm
 
 from utils.config import TransferConfig, LoadType
-from utils.file import read_json
+from utils.file import write_json
 from utils.log import ScheduleLog
 from llama2 import Llama2, LlamaType
 
-PROMPT = "There is a sentence '{}'. You should rewrite it more positive. The more positive sentence is {{"\
+PROMPT = "There is a sentence '{}'. You should rewrite it more positive. The more positive sentence is {{"
+
+END_SYMBOL = '}'
 
 def load_dataset(dataset_path: str, k: int, load_type: LoadType=LoadType.Front):
     with open(dataset_path, 'r', encoding='utf-8') as f:
@@ -25,20 +27,6 @@ def load_dataset(dataset_path: str, k: int, load_type: LoadType=LoadType.Front):
             return random.sample(datas, k)
         else:
             return None
-
-def save_result(result: list, output_path: str):
-    with open(output_path, 'w', encoding='utf-8') as f:
-        # for sentence in result:
-        #     f.write(f'"{sentence}"')
-        f.write(json.dumps(result, indent=4))
-
-def save_result_with_input(input: list, result: list, output_path: str):
-    merge = [{"0": i, "1": r} for (i, r) in zip(input, result)]
-    
-    with open(output_path, 'w', encoding='utf-8') as f:
-        # for sentence in result:
-        #     f.write(f'"{sentence}"')
-        f.write(json.dumps(merge, indent=4))
 
 def transfer_7b_chat_yelp(config: TransferConfig):
     if config.k == "":
@@ -56,12 +44,16 @@ def transfer_7b_chat_yelp(config: TransferConfig):
     bot = Llama2(PROMPT, LlamaType.Llama_7B_Chat) 
     transfer_result = []
     for sentence in tqdm(dataset, desc="Dataset: "):
-        transfer_result.append(bot.transfer(sentence))
+        # 从迁移结果中提取出有用的具体
+        result = bot.transfer(sentence)
+        # [注意] 还需要根据换行符换行 避免特殊情况
+        result = result.split(END_SYMBOL)[0].split('\n')[0]
+        transfer_result.append(result)
     s_log.log('Transfer over.')
 
     # 保存数据
-    # save_result(transfer_result, output_path)
-    save_result_with_input(dataset, transfer_result, config.output_path)
+    merge = [ {"0": i, "1": r} for (i, r) in zip(input, result) ]
+    write_json(config.output_path, merge)
     s_log.log('Save over.')
 
 def main():
