67893eea67ce5dc68e7e488cdca79014d28a5217
maillab017 <mailab017@user-NF8480M6>
2023-10-29 17:18:22 +0800

    perf: 优化transfer运行脚本

--- script/transfer.sh
+++ script/transfer.sh
@@ -1,9 +1,18 @@
-while getopts k: flag
+# while getopts k: flag
 
-do 
+# do 
+#     case "${flag}" in
+#         k) k=${OPTARG};;
+#     esac
+# done
+
+while getopts p: flag
+
+do
     case "${flag}" in
-        k) k=${OPTARG};;
+        p) path=${OPTARG};;
     esac
 done
 
-CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node=1 src/transfer.py -k=$k
+# nohup ./myprogram > foo.out 2> foo.err < /dev/null &
+CUDA_VISIBLE_DEVICES=1 nohup torchrun --nproc_per_node=1 src/transfer.py $path >output/log/log.out 2> output/log/log.err &disown
