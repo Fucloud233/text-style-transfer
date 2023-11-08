45c671a8664f5d547d1628f49e892128eee6700d
Fucloud-arch <270928474@qq.com>
2023-11-08 20:44:02 +0800

    refactor: the deserialization process

--- utils/config.py
+++ utils/config.py
@@ -5,6 +5,14 @@ from enum import Enum
 
 from utils.file import read_json, write_json
 
+class BaseConfig(object):
+    def __init__(self, file_path):
+        obj = read_json(file_path)
+        for key in vars(self):
+            if key in obj:
+                setattr(self, key, obj[key])
+    
+
 class Config:
     __config_info: dict = None
 
@@ -41,58 +49,38 @@ class RetrievalType(Enum):
     BM25 = 'bm25'
     Null = 'null'
 
+class TransferConfig(BaseConfig):
+    def __init__(self, file_path: str):
+        self.k = -1
 
-'''
-dataset_path: {
-    [
-        "dataset_path_0",
-        "dataset_path_1"
-    ]
-}
-
-'''
-
-class TransferConfig:
-    def __init__(self, k: int, load_type: LoadType, 
-            dataset_path: list[str],  output_path: str,
-            select_index: int, retrieval_type: RetrievalType
-        ):
-        self.k = k
-        
         # record the two different type of style
-        self.dataset_path = dataset_path
+        self.dataset_path = []
+        self.output_path = ""
 
-        self.output_path = output_path
-        self.load_type = load_type
+        self.load_type = None
+        self.retrieval_type = None
 
-        self.select_index = select_index
-        self.retrieval_type = retrieval_type
+        self.prompt = ""
 
-    @staticmethod
-    def from_file(file_path: str):
-        info = read_json(file_path)
+        '''
+        we will use test datasets to test
+        and use train datasets to retrieval,
+        so we can't reserve them using 'select index'
+        because they're different type of datasets
+        '''
+        # self.select_index = select_index
 
-        config = TransferConfig(
-            info['k'],
-            LoadType(info['load_type']),
-            info['dataset_path'],
-            info['output_path'],
-            info['select_index'],
-            RetrievalType[info['retrieval_type']]
-        )
+        super().__init__(file_path)
 
-        if not config.__check():
+        if not self.__check():
             raise ValueError('The number of datasets we can receive is 2!')
 
-        return config
+        # update the type 
+        self.load_type = LoadType(self.load_type)
+        self.retrieval_type = RetrievalType(self.retrieval_type)
 
     def __check(self):
-        return len(self.dataset_path) == 2 \
-            and self.select_index in [0, 1]
-    
-    @property
-    def another_index(self):
-        return self.load_type & 1
+        return len(self.dataset_path) == 2
 
 if __name__ == '__main__':
     Config.load_config_info('config.json')
