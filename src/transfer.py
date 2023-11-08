f14a86aff89b7d5bdff812752a123fac2e106df7
Fucloud-arch <270928474@qq.com>
2023-11-08 20:01:49 +0800

    feat: extend the transfer function

--- src/transfer.py
+++ src/transfer.py
@@ -7,26 +7,34 @@ import fire
 
 from tqdm import tqdm
 
-from utils.config import TransferConfig, LoadType
+from utils.config import TransferConfig, LoadType, RetrievalType
 from utils.file import write_json
 from utils.log import ScheduleLog
-from llama2 import Llama2, LlamaType
+from model.llama2 import Llama2, LlamaType
 
 PROMPT = "There is a sentence '{}'. You should rewrite it more positive. The more positive sentence is {{"
 
 END_SYMBOL = '}'
 
 def load_dataset(dataset_path: str, k: int, load_type: LoadType=LoadType.Front):
-    with open(dataset_path, 'r', encoding='utf-8') as f:
-        # 选择前k条
-        if(load_type == LoadType.Front):
-            return [f.readline().strip() for _ in range(k)]
-        # 随机选择
-        elif(load_type == LoadType.Random):
-            datas = f.read().splitlines()
-            return random.sample(datas, k)
-        else:
-            return None
+    datasets = []
+    
+    # read dataset in batches
+    for path in dataset_path:
+        with open(path, 'r', encoding='utf-8') as f:
+            # 选择前k条
+            if(load_type == LoadType.Front):
+                dataset = [f.readline().strip() for _ in range(k)]
+            # 随机选择
+            elif(load_type == LoadType.Random):
+                datas = f.read().splitlines()
+                dataset = random.sample(datas, k)
+            else:
+                continue
+
+        datasets.append(dataset)
+    
+    return datasets
 
 def transfer_7b_chat_yelp(config: TransferConfig):
     if config.k == "":
@@ -36,14 +44,23 @@ def transfer_7b_chat_yelp(config: TransferConfig):
     s_log = ScheduleLog(); s_log.start()
     s_log.log('Loading Dataset.')
     
-    # 读取k条数据集
-    dataset = load_dataset(config.dataset_path, config.k, config.load_type)    
-    s_log.log(f'Load over. (size = {len(dataset)}).')
+    # load n pieces of data from multiple datasets
+    datasets = load_dataset(config.dataset_path, config.k, config.load_type)    
+    datasets_len = [len(dataset) for dataset in datasets]
+    s_log.log(f'Load over. (size = {datasets_len}).')
 
     # 使用Llama转换
-    bot = Llama2(PROMPT, LlamaType.Llama_7B_Chat) 
+    retrieval_type = config.retrieval_type
+    if(retrieval_type == RetrievalType.Null):
+        bot = Llama2(PROMPT, LlamaType.Llama_7B_Chat) 
+    elif(retrieval_type == RetrievalType.BM25):
+        pass
+    else:
+        print("The type of retrieval is invalid!")
+        return
+            
     transfer_result = []
-    for sentence in tqdm(dataset, desc="Dataset: "):
+    for sentence in tqdm(datasets[config.select_index], desc="Dataset: "):
         # 从迁移结果中提取出有用的具体
         result = bot.transfer(sentence)
         # [注意] 还需要根据换行符换行 避免特殊情况
