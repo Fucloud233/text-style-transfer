431a6c73542d8e30a717f5cb382403a7d912b7b6
Fucloud-arch <270928474@qq.com>
2023-11-10 12:14:51 +0800

    feat: implement get take time function

--- utils/log.py
+++ utils/log.py
@@ -4,6 +4,8 @@ class ScheduleLog:
     def __init__(self, with_start=False):
         self.__begin_time = None
         self.__is_start = False
+
+        self.__pre_time = None
         
         # 自动开启计时
         if with_start:
@@ -13,6 +15,9 @@ class ScheduleLog:
         if not self.__is_start:
             self.__begin_time = time.now()
             self.__is_start = True
+
+            # init the pre_time after starting
+            self.__pre_time = self.__begin_time
         else:
             raise RuntimeWarning("The sceduleLog has started!")
         
@@ -22,6 +27,14 @@ class ScheduleLog:
 
     def log(self, info):
         print("[%02.2fs] %s" % (self.cur_time, info))
+
+    def mark(self):
+        self.__pre_time = time.now()
+
+    @property
+    def take_time(self):
+        cur_time = time.now()
+        return time.cal_time(self.__pre_time, cur_time)
 
     @property
     def cur_time(self):
