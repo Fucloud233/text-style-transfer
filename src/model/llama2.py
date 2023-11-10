6875f3bcb04f859cd1f78834d3d3323c267c9280
Fucloud-arch <270928474@qq.com>
2023-11-10 11:45:01 +0800

    refactor: use api instead sdk

--- src/model/llama2.py
+++ src/model/llama2.py
@@ -1,34 +1,58 @@
-
-from llama import Llama
+from enum import Enum
 from typing import List
 from utils.config import LlamaType
 
-MAX_BATCH_SIZE = 4
-MAX_SEQ_LEN = 256
-MAX_BATCH_SIZE = 4
-MAX_GEN_LEN = None
-TEMPERATURE = 0.5
-TOP_P = 0.9
+import requests
+
+API_URL = "localhost:5000/"
+
+class ApiType(Enum):
+    Chat = 'chat'
+    Boot = 'boot'
+    Check = 'check'
+
+    @property
+    def url(self) -> str:
+        return API_URL + self.value
+
+def convert(resp: object) -> (int, any):
+    return (resp['code'], resp['info'])
+
+def init_status():
+    resp = requests.get(ApiType.Check.url)
+    (_, is_running) = convert(resp)
+    return is_running
 
 class Llama2:
-    def __init__(self, prompt: str, model_type: LlamaType=LlamaType.Llama_7B):        
-        self.generator = Llama.build(
-            ckpt_dir=LlamaType.ckpt_dir(model_type),
-            tokenizer_path="model/tokenizer.model",
-            max_seq_len=MAX_SEQ_LEN,
-            max_batch_size=MAX_BATCH_SIZE
-        )
+    __status = init_status()
 
+    def __init__(self, prompt: str, model_type: LlamaType=LlamaType.Llama_7B):
         self.prompt = prompt
+        
+        if Llama2.__status:
+           return 
+        
+        msg = { 'llama_type': model_type.value }
+        (code, _) = convert(requests.post(ApiType.Boot.url, msg))
+        if code == 0:
+            Llama2.__status = True
 
     def transfer(self, sentence):
         prompt = self.prompt.format(sentence)
         return self.__call(prompt)
-    
+
     def __call(self, prompt):
-        return self.generator.text_completion(
-            [prompt],
-            max_gen_len=MAX_GEN_LEN,
-            temperature=TEMPERATURE,
-            top_p=TOP_P
-        )[0]["generation"]
+        msg = { "query": prompt }
+        resp = requests.post(ApiType.Chat.url, json=msg)
+
+        code, info = resp['code'], resp['info']
+
+        match code:
+            case 0: return info
+            case 1: raise ValueError(info)
+            case _: raise ValueError('Response code {} not known'.format(code))
+        
+        
+
+        
+        
