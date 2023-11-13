d8bfd0f77070bda4fd85920a8262d2410f4de997
Fucloud-arch <270928474@qq.com>
2023-11-13 13:27:03 +0800

    fix: the bug in base LLama2 bot

--- src/model/llama2.py
+++ src/model/llama2.py
@@ -28,21 +28,11 @@ def init_status():
     return is_running
 
 class Llama2:
-    __status = init_status()
-
-    def __init__(self, prompt: str, model_type: LlamaType=LlamaType.Llama_7B):
+    def __init__(self, prompt: str):
         self.prompt = prompt
-        
-        if Llama2.__status:
-           return 
-        
-        msg = { 'llama_type': model_type.value }
-        (code, _) = convert(requests.post(ApiType.Boot.url, msg))
-        if code == 0:
-            Llama2.__status = True
 
     def transfer(self, sentence):
-        prompt = self.prompt.format(sentence)
+        prompt = self.prompt.format(sentence=sentence)
         return (self._call(prompt), prompt)
     
     def _call(self, prompt):
