f8aa73375b012c15edcfa5646d08f1e279aca8c9
fucloud-win11 <270928474@qq.com>
2023-11-13 16:13:40 +0800

    fix: migrate to new openai api

--- src/bot.py
+++ src/bot.py
@@ -4,18 +4,22 @@ sys.path.append('.')
 from utils.config import Config
 import openai
 
-openai.api_key = Config.openai_key
-
+# Migration Guide: https://github.com/openai/openai-python/discussions/742
 
 class Bot:
     model_type = "gpt-3.5-turbo-0613"
+
+    client = openai.OpenAI(
+        api_key=Config.openai_key
+    )
     
     @classmethod
-    def ask(self, system_prompt: str, prompt: str) -> str:
-        response = openai.ChatCompletion.create(
+    def ask(self, prompt: str, system_prompt: str="") -> str:
+        response = Bot.client.chat.completions.create(
             model=Bot.model_type,
-            request_timeout=5,
+            timeout=5,
             temperature=0,
+            stream=False,
             messages=[
                 {
                     "role": "system", 
@@ -28,5 +32,5 @@ class Bot:
             ]
         )
 
-        return response['choices'][0]['message']['content']
+        return response.choices[0].message.content
     
