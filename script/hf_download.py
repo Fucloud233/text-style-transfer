39c84dfae006a4e3a0a87bb1d0fa2c541c68f7cf
Fucloud-arch <270928474@qq.com>
2023-11-17 10:47:34 +0800

    upload: download roberta

--- script/hf_download.py
+++ script/hf_download.py
@@ -1,17 +1,28 @@
-from huggingface_hub import hf_hub_download, try_to_load_from_cache
+from huggingface_hub import hf_hub_download, snapshot_download
 
 # https://huggingface.co/edugp/kenlm
 # https://github.com/huggingface/huggingface_hub
 
 
-def download():
+def download_tmp():
     hf_hub_download(repo_id="edugp/kenlm", filename="wikipedia/en.arpa.bin")
     hf_hub_download(repo_id="edugp/kenlm", filename="wikipedia/en.sp.model")
     hf_hub_download(repo_id="edugp/kenlm", filename="model.py")
 
+def download(repo_id: str, filename: str):
+    hf_hub_download(repo_id=repo_id, filename=filename)
+
+def download_repo(repo_id: str):
+    snapshot_download(repo_id=repo_id)
+
+def download_roberta():
+    # when I want to use it, it will be downloaded
+    from transformers import RobertaTokenizer, RobertaModel
+    tokenizer = RobertaTokenizer.from_pretrained('roberta-large')
+    model = RobertaModel.from_pretrained('roberta-large')
 
 if __name__ == '__main__':
     # file_path = try_to_load_from_cache(repo_id="edugp/kenlm", filename="wikipedia/en.arpa.bin")
     # print(file_path)
 
-    download()
+    download_repo('roberta-large')
