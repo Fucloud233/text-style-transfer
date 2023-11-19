c28736c9f8865f666d162e98054f61f50a680d67
Fucloud-arch <270928474@qq.com>
2023-11-19 22:26:32 +0800

    perf: the message generation

--- src/bot.py
+++ src/bot.py
@@ -6,6 +6,12 @@ import openai
 
 # Migration Guide: https://github.com/openai/openai-python/discussions/742
 
+def generate_msg(role: str, content: str):
+    return {
+        "role": role,
+        "content": content
+    }
+
 class Bot:
     model_type = "gpt-3.5-turbo-0613"
 
@@ -14,22 +20,19 @@ class Bot:
     )
     
     @classmethod
-    def ask(self, prompt: str, system_prompt: str="") -> str:
+    def ask(self, prompt: str, system_prompt: str=None) -> str:
+        # generate the messages
+        messages = []
+        if system_prompt != None:
+            messages.append(generate_msg("system", system_prompt))
+        messages.append(generate_msg("user", prompt))
+
         response = Bot.client.chat.completions.create(
             model=Bot.model_type,
             timeout=5,
             temperature=0,
             stream=False,
-            messages=[
-                {
-                    "role": "system", 
-                    "content": system_prompt
-                },
-                {
-                    "role": "user", 
-                    "content": prompt
-                }
-            ]
+            messages=messages
         )
 
         return response.choices[0].message.content
