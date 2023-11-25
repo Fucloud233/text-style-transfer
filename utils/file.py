f39aae238d72d6f9a655299ad2f6e23452d01fe4
Fucloud-arch <270928474@qq.com>
2023-11-25 14:55:10 +0800

    style: add typing hint for return value

--- utils/file.py
+++ utils/file.py
@@ -18,7 +18,7 @@ def write_lines(file_path: str, lines: List[str]):
         for line in lines:
             f.write(line + '\n')
 
-def read_json(file_path: str):
+def read_json(file_path: str) -> dict:
     with open(file_path, 'r') as f:
         return json.load(f)
     
